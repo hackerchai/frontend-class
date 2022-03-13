4b60a712d6d672158efd3060c05c25b020ec6752
hackerchai <i@hackerchai.com>
2022-03-14 00:29:59 +0800

    initial comit
    Signed-off-by: hackerchai <i@hackerchai.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "@components/layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Hackerchai</title>
+        <meta
+          name="Description"
+          content="Hackerchai's frontend lab"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp
