a6890df1f6e943f1100f04d2dafeec123a675efb
hackerchai <i@hackerchai.com>
2022-03-24 09:47:56 +0800

    feat: take down contents
    Signed-off-by: hackerchai <i@hackerchai.com>

--- pages/homeworks/ch03.js
+++ pages/homeworks/ch03.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import exImage from '../../public/images/ta.jpg'
+import exImage from '../../public/images/messi.jpeg'
 
 
 export default function ch_03() {
@@ -7,29 +7,29 @@ export default function ch_03() {
         <div className="grid md:grid-cols-2 gap-6 md:grid-cols-2">
             <div>
                 <section className="mb-6">
-                    <h2 className="mb-3 text-xl font-bold">蛤是谁?</h2>
+                    <h2 className="mb-3 text-xl font-bold">球王是谁?</h2>
 
                     <p>
-                        <a href="https://baike.baidu.com/item/%E6%B1%9F%E6%B3%BD%E6%B0%91/115299">江泽民</a>（1926年8月17日－），男，汉族，祖籍安徽省徽州府婺源县江湾，江苏省扬州市人。1943年考入中央大学电机工程系二年级（今南京大学）并开始参加地下党领导的学生运动，1946年4月加入中国共产党，1947年毕业于上海交通大学电机系。曾任中华人民共和国最高领导人，中国共产党中央委员会总书记、中华人民共和国主席、中国共产党中央军事委员会主席、中华人民共和国中央军事委员会主席，中国共产党中央政治局委员、常委等职务。
+                        <a href="https://baike.baidu.com/item/%E5%88%A9%E6%98%82%E5%86%85%E5%B0%94%C2%B7%E6%A2%85%E8%A5%BF/3965248">梅西</a> 利昂内尔·安德烈斯·“利奥”·梅西·库西提尼（西班牙语：Lionel Andrés "Leo" Messi Cuccittini，西班牙语发音：[ljoˈnel anˈdɾes ˈmesi] （关于这个音频文件 聆听）；1987年6月24日－），简称梅西，生于阿根廷圣菲省罗萨里奥，现正效力于法甲球会巴黎圣日耳门，同时担任阿根廷国家足球队队长，司职翼锋、前锋及进攻中场。[2]目前他共获得7座金球奖、6次世界足球先生及6座欧洲金靴奖，3项均为世界记录。团体奖方面，他共获得10次西班牙甲组足球联赛冠军、4次欧洲冠军联赛冠军、1次奥运会足球赛冠军、1次美洲国家杯冠军、1次国际足联U-20世界杯冠军、2次南美超级德比杯冠军及2014年国际足协世界杯亚军。
                     </p>
                 </section>
 
                 <section>
-                    <h2 className="mb-3 text-xl font-bold">为什么香港记者跑得快?</h2>
+                    <h2 className="mb-3 text-xl font-bold">7获金球，前无古人</h2>
                     <p>
-                        江泽民怒斥香港记者，指2000年10月27日，香港有线电视女记者张宝华在记者会上詢問时任中共中央总书记、中国国家主席江泽民是否钦点董建华两年後连任香港特别行政区行政长官，江泽民不满「钦点」一词，澄清自己只是「支持」，並斥责张宝华之事。最终2002年香港行政長官選舉董建华自动当选。此事對膜蛤文化影響甚深。
+                    根据西媒消息，当地时间11月29日晚间，由《法国足球》杂志主办的金球奖年度颁奖盛典在法国巴黎举行，效力于巴黎圣日耳曼俱乐部的阿根廷球星莱昂内尔·梅西获得了象征2021年度最佳球员的金球奖，这是梅西职业生涯第7次收获这一奖项，再度刷新了由自己保持的赢得金球奖次数最多的纪录。梅西在2021年帮助巴塞罗那足球俱乐部赢得了西班牙国王杯冠军，并以队长身份帮助阿根廷国家队赢得了美洲杯冠军。2021年8月，梅西正式从巴塞罗那足球俱乐部转会至巴黎圣日耳曼足球俱乐部。
                     </p>
                 </section>
             </div>
 
             <section className="mb-6">
-                <Image src={exImage} width="200" height="200" alt="江泽民"/>
+                <Image src={exImage} width="1897" height="1384" alt="梅西"/>
             </section>
             <section>
-                <embed src="/assets/jounalist.mp3" autostart="true" width="0" height="0"/>
+                <embed src="/assets/barca.mp3" autostart="true" width="0" height="0"/>
             </section>
             <section>
-                <marquee bgcolor="green" width="285" height="100" direction="up"><font 信息 color="white" size="6" center="center">我给长者续一秒+1s</font></marquee>
+                <marquee bgcolor="green" width="250" height="100" direction="up"><font 信息 color="white" size="6" center="center">梅西球王！！！</font></marquee>
             </section>
 
         </div>
