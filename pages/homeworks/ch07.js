712284d74b275c5c9c261972f218fff98c58374d
hackerchai <i@hackerchai.com>
2022-03-28 01:46:42 +0800

    feat: update slide & animation
    Signed-off-by: hackerchai <i@hackerchai.com>

---
+++ pages/homeworks/ch07.js
@@ -0,0 +1,8 @@
+import animation from '../../styles/animation.module.css'
+export default function ch_07() {
+    return (
+        <div className="flex flex-col justify-center items-center space-y-3">
+            <h1 className={animation.h1}>闷声大发财</h1>
+        </div>
+    )
+}
