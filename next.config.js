60ed178423ce0e6b13f70feada9cdf1ccbe43c61
hackerchai <i@hackerchai.com>
2023-10-26 23:14:31 +0800

    refactor: bump next.js version
    Signed-off-by: hackerchai <i@hackerchai.com>

--- next.config.js
+++ next.config.js
@@ -1,39 +1,4 @@
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-function useEsbuildMinify(config, options) {
-	const { minimizer } = config.optimization;
-	const terserIndex = minimizer.findIndex(
-		minifier => minifier.constructor.name === 'TerserPlugin',
-	);
-	
-	minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
-}
-
-function useEsbuildLoader(config, options) {
-	const { rules } = config.module;
-	const rule = rules.find(rule => rule.test.test('.js'));
-
-	rule.use = {
-		loader: 'esbuild-loader',
-		options,
-	};
-}
-
-module.exports = {
-	webpack: (config, { webpack }) => {
-		config.plugins.push(
-			new webpack.ProvidePlugin({
-				React: 'react',
-			}),
-		);
-
-		useEsbuildMinify(config);
-		useEsbuildLoader(config, {
-			// Specify `tsx` if you're using TypeScript
-			loader: 'jsx',
-			target: 'es2017',
-		});
-
-		return config;
-	},
-};
+module.exports = nextConfig
--- pages/homeworks/ch07.js
+++
@@ -1,8 +0,0 @@
-import animation from '../../styles/animation.module.css'
-export default function ch_07() {
-    return (
-        <div className="flex flex-col justify-center items-center space-y-3">
-            <h1 className={animation.h1}>闷声大发财</h1>
-        </div>
-    )
-}
