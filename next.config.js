3d133f2cb022c12bebc1cecbe43b2a1653ab7152
hackerchai <i@hackerchai.com>
2023-10-26 22:31:25 +0800

    chore: update lock file
    Signed-off-by: hackerchai <i@hackerchai.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+function useEsbuildMinify(config, options) {
+	const { minimizer } = config.optimization;
+	const terserIndex = minimizer.findIndex(
+		minifier => minifier.constructor.name === 'TerserPlugin',
+	);
+	
+	minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+}
+
+function useEsbuildLoader(config, options) {
+	const { rules } = config.module;
+	const rule = rules.find(rule => rule.test.test('.js'));
+
+	rule.use = {
+		loader: 'esbuild-loader',
+		options,
+	};
 }
 
 module.exports = {
-}
+	webpack: (config, { webpack }) => {
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				React: 'react',
+			}),
+		);
+
+		useEsbuildMinify(config);
+		useEsbuildLoader(config, {
+			// Specify `tsx` if you're using TypeScript
+			loader: 'jsx',
+			target: 'es2017',
+		});
+
+		return config;
+	},
+};
